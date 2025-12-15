12231bfeab374a421fb8ed82073f7208e7fb7015
nuna101 <nunajidapanak@gmail.com>
2025-12-15 15:57:58 +0700

    components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
       <h1 className='text-2xl text-red-500'>Welcome to Lakorn Vidva 2026!!</h1>
+      <Header />
     </>
   )
 }
